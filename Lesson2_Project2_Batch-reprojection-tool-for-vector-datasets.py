0454ba0a42718435e5255a5cd6cce62cf2070f1b
Abraão Nascimento <abraaoalvesdonascimento@gmail.com>
2016-02-28 22:45:08 -0300

    Update Lesson2_Project2_Batch-reprojection-tool-for-vector-datasets.py

--- Lesson2_Project2_Batch-reprojection-tool-for-vector-datasets.py
+++ Lesson2_Project2_Batch-reprojection-tool-for-vector-datasets.py
@@ -1,10 +1,21 @@
+import time
+from time import gmtime, strftime
+
 from arcpy import env,  Describe, GetMessages, GetParameterAsText, ListFeatureClasses, Project_management, SpatialReference
 
-#input_folder = "C:\\learnPython\\data\\lesson2_2"
-#input_file = "C:\\learnPython\\data\\lesson2_2\\StateRoutes.shp"
+# Start scrpit
+start_time = time.time()
+
+# Get the parameters of project2 tool
 input_folder = GetParameterAsText(0)
 input_file = GetParameterAsText(1)
 
+# input_folder = "C:\\learnPython\\data\\lesson2_2"
+# input_file = "C:\\learnPython\\data\\lesson2_2\\StateRoutes.shp"
+
+print("Executing: Project2 tool " + str(input_folder) + " " + str(input_file))
+print ("Start Time: " + strftime("%a, %d %b %Y %H:%M:%S 2016", gmtime()))
+print ("Running a script Project2-Batch-reprojection-tool-for-vector-datasets")
 # Get a list of all feature classes in the standard folder
 env.workspace = input_folder
 featureClass = ListFeatureClasses()
@@ -13,6 +24,8 @@ featureClass = ListFeatureClasses()
 desc_standard_file = Describe(input_file)
 sr_standard_file = desc_standard_file.SpatialReference
 
+# list for keep the reproject files names
+l = []
 try: 
 
     #Loop through all feature classes in standar folder
@@ -26,7 +39,7 @@ try:
         # the standard  spatial reference, show message
         # about this and pass to next feature
         if sr.name == sr_standard_file.name:
-            print "This file already have the Standard spacial reference"
+            print ("This file: " + str(feature) + ", already have the standard spatial reference")
             pass
 
         # If the name of spatial reference is different to
@@ -46,7 +59,12 @@ try:
             # reproject file
             Project_management(feature, input_folder + out_name, coor_system)
 
+            print ("Projected: " + str(feature))
 # If happen some error 
 except:
     # Show messages for error
     print GetMessages()
+
+print "Completed script Project2"
+end_time = time.time()
+print ("End Time: " + strftime("%a, %d %b %Y %H:%M:%S 2016", gmtime()) + ", Elaped Time: " + str(end_time - start_time)[:-5] + " seconds")
