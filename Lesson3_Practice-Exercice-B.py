1a1e8e330d704bc84030c23080067498aa5c643b
Abraão Nascimento <abraaoalvesdonascimento@gmail.com>
2016-03-06 23:10:56 -0300

    Update Lesson3_Practice-Exercice-B.py

--- Lesson3_Practice-Exercice-B.py
+++ Lesson3_Practice-Exercice-B.py
@@ -1,4 +1,4 @@
-# TESTE 1
+#
 # ----------------------- FUNCTION TO CALCULATE THE PERCENTAGE VALUE-----------------------------------
 
 def percentage(totalValue, percentageValue):
@@ -22,23 +22,33 @@ parkAndRideField = "HasTwoParkAndRides"   # Name of column for storing the Park 
 # Name of park/ride geography data
 parkAndRide = "ParkAndRide"
 
+
 with UpdateCursor(cityBoundaries, (nameField, parkAndRideField)) as cityRows:
     for city in cityRows:
 
-        name = city[0]
+        nameCity = city[0]
         # Create a query string for the current city    
         #cityIDString = city[0]
-        queryString = '"' + nameField + '" = ' + "'" + name + "'"
+        queryString = '"' + nameField + '" = ' + "'" + nameCity + "'"
         # Make a feature layer of just the current city polygon    
         MakeFeatureLayer_management(cityBoundaries, "CurrentCityLayer", queryString)
+        MakeFeatureLayer_management(parkAndRide, "ParkAndRide_lry")
+
+        SelectLayerByLocation_management("ParkAndRide_lry", "CONTAINED_BY", "CurrentCityLayer")
 
         # Get the total value of cities in CityBoundaries 
         city = GetCount_management("CurrentCityLayer")
         totalCity = int(city.getOutput(0))
 
+        Park = GetCount_management("ParkAndRide_lry")
+        totalPark = int(city.getOutput(0))
+
         print queryString
         print totalCity
 
+        print totalPark
+
+        Delete_management("CurrentCityLayer")
         Delete_management("CurrentCityLayer")
 
         break
