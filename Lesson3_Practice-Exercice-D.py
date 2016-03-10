555b3b48cf281f71cad309c4ba0b32e8686dcc8a
Abraão Nascimento <abraaoalvesdonascimento@gmail.com>
2016-03-10 20:31:31 -0300

    Update Lesson3_Practice-Exercice-D.py

--- Lesson3_Practice-Exercice-D.py
+++ Lesson3_Practice-Exercice-D.py
@@ -1,30 +1,50 @@
-#-- TESTE1
-from arcpy import env, Delete_management, MakeFeatureLayer_management, CopyFeatures_management, SelectLayerByLocation_management
+#
+#---------------SELECTS ALL THE PARK AND RIDE FACILITIES IN A GIVEN CITY AND SAVES THEN OUT A NEW FEATURE CLASS-----------------
+from arcpy import env, Delete_management, CopyFeatures_management, MakeFeatureLayer_management, SelectLayerByLocation_management
 
 # Setting workspace
 env.workspace = "C:\\learnPython\\data\\Lesson3PracticeExerciseD\\Washington.gdb"
 
-# Name of geography data of park and rides
+# Name of feature park and rides
 parkAndRide = "ParkAndRide"
 
+# Name of featue city boundaries 
 cityBoundaries = "CityBoundaries"
+
+# Name of field with the name of cities
 nameField = 'NAME'
 
+# Name of city for select layer by location
 nameCity = 'Federal Way'
 
-nameCityLayer = nameCity + '_lyr'
+# Try selects all the park and ride facilities in a given city and saves them out to a new feature class
+try:
 
-nameOut = nameCity + '_ParkAndRide'
+    # SQL to select only one city
+    queryString = '"' + nameField + '" = ' + "'" + nameCity + "'" # It means the same that: " nameField + " = ' nameCity '
+    
+    # Name of new layer of city
+    nameCityLayer = nameCity.replace(" ","_") + '_lyr'
 
-queryString = '"' + nameField + '" = ' + "'" + nameCity + "'" # It means that: " nameField + " = ' nameCity '
+    # Make a feature layers
+    MakeFeatureLayer_management(parkAndRide,"parkAndRide_lry")      
+    MakeFeatureLayer_management(cityBoundaries, nameCityLayer, queryString)
 
-MakeFeatureLayer_management(parkAndRide,"ParkAndRide_lry")
-        
-MakeFeatureLayer_management(cityBoundaries, nameCityLayer, queryString)
+    # Select all park and ride into of only city
+    SelectLayerByLocation_management("parkAndRide_lry", "CONTAINED_BY", nameCityLayer)
 
-SelectLayerByLocation_management("ParkAndRide_lry", "CONTAINED_BY", nameCityLayer)
+    # Name of new feature class
+    nameFeatureOut = nameCity + '_ParkAndRide'
 
-CopyFeatures_management("parkAndRide_500_lyr", nameOut)
+    # Create a new feature class only with park and ride into of city
+    CopyFeatures_management("parkAndRide_lry", nameOut)
 
-Delete_management("ParkAndRide_lry")
-Delete_management(nameCityLayer)
+    # Detele feature layers
+    Delete_management("parkAndRide_lry")
+    Delete_management(nameCityLayer)
+
+# If happen some error
+except:
+
+    # Show the message
+    print 'It was not possible selects all the park and ride facilities in a given city and saves them out to a new feature class'
