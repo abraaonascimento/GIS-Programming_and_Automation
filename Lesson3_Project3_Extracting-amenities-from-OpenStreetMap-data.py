5218b4c69917dedd7c4c6a686f66f5cff8705d0d
Abraão Nascimento <abraaoalvesdonascimento@gmail.com>
2016-03-12 20:00:46 -0300

    Update Lesson3_Project3_Extracting-amenities-from-OpenStreetMap-data.py

--- Lesson3_Project3_Extracting-amenities-from-OpenStreetMap-data.py
+++ Lesson3_Project3_Extracting-amenities-from-OpenStreetMap-data.py
@@ -1,5 +1,5 @@
-# MAKES A SEPARATE SHAPEFILE FOR EACH OF THESE TYPES OF AMENITIES (SCHOOLS, HOSPITALS, PLACES OF WORSHIP) WITHIN THE BOUNDARY OF EL SALVADOR ----------------------------------
-from arcpy import env, AddField_management, CopyFeatures_management,  MakeFeatureLayer_management, UpdateCursor
+#------------MAKES A SEPARATE SHAPEFILE FOR EACH OF THESE TYPES OF AMENITIES (SCHOOLS, HOSPITALS, PLACES OF WORSHIP) WITHIN THE BOUNDARY OF EL SALVADOR------------
+from arcpy import env, AddField_management, CopyFeatures_management,  Delete_management, MakeFeatureLayer_management, SelectLayerByLocation_management, UpdateCursor
 from arcpy.da import UpdateCursor
 
 # Setting the workspace
@@ -9,37 +9,45 @@ env.workspace = "C:\\learnPython\\data\\Project3"
 centralAmericaBoundaries = "CentralAmerica.shp"
 osmPoints = "OSMpoints.shp"
 
+# The name field of geographic data of points by Open Street Map
+nameFieldAmenity = "Amenity"
+nameFieldCountry = "NAME"
+
 # Input the names of amenities and the name of country
 amenities = ["school","hospital","place_of_worship"]
 country = "El Salvador"
 
-# The name field of geographic data of points by Open Street Map
-nameFieldAmenity = "Amenity"
-
 try:
-    # For each type amenities in amenities list
+    #
+    queryStringCountry = '"' + nameFieldCountry + '" = ' + "'"  + country + "'"
+    nameCountryLayer = country + "lyr"
+    MakeFeatureLayer_management(centralAmericaBoundaries, nameCountryLayer, queryStringCountry)
+
     for typeAmenities in amenities:
 
         # SQL pick up the type amenities
-        queryString = '"' + nameFieldAmenity + '" = ' + "'"  + typeAmenities + "'"
+        queryStringAmenities = '"' + nameFieldAmenity + '" = ' + "'"  + typeAmenities + "'"
 
         # The name of new feature layer 
         nameAmenitiesLayer = typeAmenities + "lyr"
-    
+
         # Mame a feature layer of type amenities 
-        MakeFeatureLayer_management(osmPoints, nameAmenitiesLayer, queryString)
+        MakeFeatureLayer_management(osmPoints, nameAmenitiesLayer, queryStringAmenities)
+
+        # Select only amanities into the country El Salvador
+        SelectLayerByLocation_management(nameAmenitiesLayer, "CONTAINED_BY", nameCountryLayer)
 
         # Makes a separate shapefile for each types of amenities 
         CopyFeatures_management(nameAmenitiesLayer, typeAmenities)
 
-        # get the new separete shapefile
+        # Get the new separete shapefile
         amenitiesTable = typeAmenities + '.dbf'
+        
+        #
+        newField = "source"
 
         # Add new field called 'source'
         AddField_management(amenitiesTable, newField, "TEXT", 100)
-
-        #
-        newField = "source"
         
         with UpdateCursor(typeAmenities + ".shp", newField) as amenitiesRows:
 
