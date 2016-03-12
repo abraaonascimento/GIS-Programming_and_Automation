017c6be6d63aa8594fd792dfa8498478302a2282
Abraão Nascimento <abraaoalvesdonascimento@gmail.com>
2016-03-12 00:12:53 -0300

    Update Lesson3_Project3_Extracting-amenities-from-OpenStreetMap-data.py

--- Lesson3_Project3_Extracting-amenities-from-OpenStreetMap-data.py
+++ Lesson3_Project3_Extracting-amenities-from-OpenStreetMap-data.py
@@ -1,10 +1,8 @@
-# TESTE 1
+# TESTE 2
 
 #-----MAKES A SEPARATE SHAPEFILE FOR EACH OF THESE TYPES OF AMENITIES (SCHOOLS, HOSPITALS,
 # PLACES OF WORSHIP) WITHIN THE BOUNDARY OF EL SALVADOR ----------------------------------
-
-from arcpy import env, CopyFeatures_management,  MakeFeatureLayer_management
-import arcpy
+from arcpy import env, AddField_management, CopyFeatures_management,  MakeFeatureLayer_management, UpdateCursor
 
 # Setting the workspace
 env.workspace = "C:\\learnPython\\data\\Project3"
@@ -20,17 +18,46 @@ country = "El Salvador"
 # The name field of geographic data of points by Open Street Map
 nameFieldAmenity = "Amenity"
 
+# Try makes a separate shapefile for each types of amenities 
+try:
+    
+    # For each type amenities in amenities list
+    for typeAmenities in amenities:
+
+        # SQL pick up the type amenities
+        queryString = '"' + nameFieldAmenity + '" = ' + "'"  + typeAmenities + "'"
+
+        # The name of new feature layer 
+        nameAmenitiesLayer = typeAmenities + "lyr"
+    
+        # Mame a feature layer of type amenities 
+        MakeFeatureLayer_management(osmPoints, nameAmenitiesLayer, queryString)
+
+        # Makes a separate shapefile for each types of amenities 
+        CopyFeatures_management(nameAmenitiesLayer, typeAmenities)
+
+# If Happen some error
+except:
+
+    # Show the message
+    print 'It was not possible makes a separete shapefile for amenities'
+
+
 # For each type amenities in amenities list
 for typeAmenities in amenities:
 
-    # SQL pick up the type amenities
-    queryString = '"' + nameFieldAmenity + '" = ' + "'"  + typeAmenities + "'"
+    # get the new separete shapefile
+    amenitiesTable = typeAmenities + '.dbf'
 
-    # The name of new feature layer 
-    nameAmenitiesLayer = typeAmenities + "lyr"
-    
-    # Mame a feature layer of type amenities 
-    MakeFeatureLayer_management(osmPoints, nameAmenitiesLayer, queryString)
+    #
+    newField = "source"
 
-    # Makes a separate shapefile for each types of amenities 
-    CopyFeatures_management(nameAmenitiesLayer, typeAmenities)
+    # Add new field called 'source'
+    AddField_management(amenitiesTable, newField, "TEXT", 100)
+
+    #
+    with UpdateCursor(typeAmenities, (newField)) as amenitiesRows:
+
+        for row in amenitiesRows:
+
+            row[0] = "OpenStreetMap"
